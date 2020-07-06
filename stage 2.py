66fa105f4e97c9ab499c3e724a5e619eaae3137b
ajmalbinnizam <ajmalnizam369@gmail.com>
2020-07-07 00:44:13 +0530

    stage 3 started

---
+++ stage 2.py
@@ -0,0 +1,40 @@
+# print(type("int"))
+# print(type(394))
+# print(type(2.71))
+
+# a = 10
+# b = 20
+# c = 30
+# a = c
+# b = a
+# d = c
+# c = a
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+# user_name = input()
+# print('Hello, ' + user_name)
+# day = int(input( "enter the day"))  # 4
+# month = input("enter the month")     # October
+# print('Cinnamon roll day is celebrated on', month, day)
+# # Cinnamon roll day is celebrated on October 4
+
+# n = int(input())
+# word = input()
+# print(n * word)
+
+# user = input()
+# print("Hello, " + user)
+
+# print('Hello! My name is Aid.')
+# print('I was created in 2020.')
+# print('Please, remind me your name.')
+# user = input()
+# print('What a great name you have, ' + user + '!')
+
+# print("""\
+# O X X
+# O X O
+# X O X""")
