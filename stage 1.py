d244a1633387f7f13b46ca485a4c8ec44cba7e73
ajmalbinnizam <ajmalnizam369@gmail.com>
2020-07-06 22:55:27 +0530

    chat bot upto stage 2

---
+++ stage 1.py
@@ -0,0 +1,27 @@
+print("* * * *")
+
+print("*     *")
+
+print("*     *")
+
+print("* * * *")
+
+dt="123456"
+print(len(dt))
+print(dt.upper())
+print(dt[2:-1])
+
+print('word = word.replace("\u0301", " ")')  #  delete stress symbols from the word
+print('Those in favor, say "aye"')
+
+print("Wow! It's amazing!")
+
+print('It\'s time for tea!' )
+
+print("He said: \"So what?\"")
+
+print('word = word.replace("\u0301", "")')  # delete stress symbols from the word
+print("\\")
+print()
+a="""word = word.replace("\u0301", "")"""
+print(a)
